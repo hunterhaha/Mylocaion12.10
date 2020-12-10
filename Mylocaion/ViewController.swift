5ddccef2a3b2ea10930532004f964f0435ebc977
hunter <hunter0697@gmail.com>
2020-12-10 09:36:35 +0800

    Initial Commit

---
+++ Mylocaion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mylocaion
+//
+//  Created by NDHU_CSIE on 2020/12/10.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
